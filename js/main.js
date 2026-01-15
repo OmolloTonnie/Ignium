c570ccc5d6fbc6f6d7594ed5ccf2f1e665ea6a1d
Tonnie <omollotonnie@gmail.com>
2026-01-15 17:39:34 +0300

    update header

--- js/main.js
+++ js/main.js
@@ -119,3 +119,12 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+<script>
+const mobileMenu = document.getElementById('mobile-menu');
+const navList = document.querySelector('.nav-list');
+
+mobileMenu.addEventListener('click', () => {
+  navList.classList.toggle('active');
+  mobileMenu.classList.toggle('open');
+});
+</script>
